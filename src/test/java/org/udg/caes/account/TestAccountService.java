b95433e600cbd3eb643cafbc5c6b7cd573c1a447
Edu Aymerich <eduardaymerich@gmail.com>
2018-11-05 18:08:27 +0100

    Fet

--- src/test/java/org/udg/caes/account/TestAccountService.java
+++ src/test/java/org/udg/caes/account/TestAccountService.java
@@ -1,20 +1,42 @@
 package org.udg.caes.account;
 
-import mockit.Expectations;
-import mockit.Verifications;
+import mockit.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import mockit.Tested;
-import mockit.Mocked;
 
-
+import static javafx.beans.binding.Bindings.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestAccountService {
 
+    @Mocked
+    Account acc1;
+    Account acc2;
+
+    @BeforeEach
+    void setup() {
+        acc1 = new Account( "acc1", 200 );
+        acc2 = new Account( "acc2", 400 );
+    }
 
   @Test
   void testTransfer(@Tested AccountService as, @Mocked AccountManager am)  {
-    throw new UnsupportedOperationException("Not implemented yet");
+
+      new Expectations() {{
+          am.findAccount("acc1"); result = acc1;
+          am.findAccount("acc2"); result = acc2;
+      }};
+
+      as.setAccountManager( am );
+      as.transfer("acc1", "acc2", 100);
+
+      new Verifications() {{
+          acc1.credit( 100 ); times = 1;
+          acc2.debit( 100 ); times = 1;
+      }};
+
+      assertEquals( acc1.getBalance(), acc2.getBalance() );
+
   }
 }
